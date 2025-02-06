a45df6712f0b57ab2e32f01efaeac822e7baebdc
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-06 02:08:12 -0800

    Remove Next.js build artifacts and add .env to .gitignore

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 import "dotenv/config";
+import Image from 'next/image';
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 console.log(baseUrl);
@@ -29,7 +30,9 @@ const Home = async () => {
     return redirect('/auth');
   }
 
-  return <div>Home</div>;
+  return <div className="w-full min-h-screen bg-gradient-to-tr from-green-400 to-blue-500 flex justify-center items-center">
+    <Image height={400} width={400} src={'/to-do-list.png'} alt='todo-list'></Image>
+  </div>;
 };
 
 export default Home;
